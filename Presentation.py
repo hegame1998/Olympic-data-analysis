fe736439aecbf0182f9c8bbf9483d5758c01e008
hegame1998 <158206364+hegame1998@users.noreply.github.com>
2024-02-21 17:07:14 +0100

    Update Presentation.py

--- Presentation.py
+++ Presentation.py
@@ -5,3 +5,15 @@ import numpy as np
 
 st.header("Presentation of Final Project")
 st.subheader("Programming & Database")
+
+
+val_count  = df['column_name'].value_counts()
+fig = plt.figure(figsize=(10,5))
+sns.barplot(val_count.index, val_count.values, alpha=0.8)
+fig.title('Some title')
+fig.ylabel('y label', fontsize=12)
+fig.xlabel('x label', fontsize=12)
+
+
+# Add figure in streamlit app
+st.pyplot(fig)
