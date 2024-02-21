e0173afa9980429a4e042832b0fc4f4040c060a9
hegame1998 <158206364+hegame1998@users.noreply.github.com>
2024-02-21 16:36:02 +0100

    Update Presentation.py

--- Presentation.py
+++ Presentation.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
 import streamlit as st
 
 st.header("Presentation of Final Project")
