0868665fb3c443ee5a0ba04306e9c8548d19f30f
hegame1998 <158206364+hegame1998@users.noreply.github.com>
2024-02-21 17:18:32 +0100

    Update Presentation.py

--- Presentation.py
+++ Presentation.py
@@ -6,6 +6,7 @@ import streamlit as st
 st.header("Presentation of Final Project")
 st.subheader("Programming & Database")
 
+<br>
 
 athlete_df = pd.read_csv('https://raw.githubusercontent.com/MaH1996SdN/programming_project/master/athlete_events.csv')
 st.write("The raw data of athlete of olympic:")
