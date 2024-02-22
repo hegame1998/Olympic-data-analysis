185d8df7837c9d347af1518f0e2887cb5c704c55
hegame1998 <158206364+hegame1998@users.noreply.github.com>
2024-02-22 20:32:49 +0100

    Add files via upload

---
+++ Presentation/Presentation(Hengame_Shahriyari).py
@@ -0,0 +1,25 @@
+import pandas as pd
+import streamlit as st
+
+
+
+st.set_page_config(
+    page_title="Presentation Of Hengame Shahriyari",
+    page_icon="📈",
+)
+
+st.write("# Presentation for Analyse Athletes of Olympic!")
+st.divider()
+st.markdown(
+    """
+    It is my presentation for the lecture of Programming.
+
+    **👈 Select the exact information that you want to see from sidebar** to see some my project and simulation!    
+"""
+)
+
+st.empty()
+st.write("#### Author : Hengame Shahriyari")
+
+
+st.sidebar.success("Select the category that you want to view")
